70a56133ebd2d2d8f7d5c32e7c2d0c129c04fa1f
jit-webapp <webapp.jit@gmail.com>
2026-02-06 19:22:32 +0700

    Add core/storage.js with cloud and local storage operations

---
+++ core/storage.js
@@ -0,0 +1,19 @@
+// Cloud Storage Operations
+
+function uploadToCloud(file) {
+    // Code to upload file to cloud storage
+}
+
+function downloadFromCloud(fileName) {
+    // Code to download file from cloud storage
+}
+
+// Local Storage Operations
+
+function saveToLocal(file) {
+    // Code to save file to local storage
+}
+
+function readFromLocal(fileName) {
+    // Code to read file from local storage
+}
